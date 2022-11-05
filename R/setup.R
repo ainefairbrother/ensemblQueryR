8634c152eee5407689c5b172150123ed53be220a
Aine Fairbrother-Browne <aine.fairbrother-browne.18@ucl.ac.uk>
2022-11-05 12:53:34 +0000

    Add ./R dir

---
+++ R/setup.R
@@ -0,0 +1,10 @@
+library(devtools)
+library(usethis)
+
+## setup git
+# git init
+# git add .
+# git commit -m "Initial commit"
+# git remote add origin https://github.com/ainefairbrother/ensemblQueryR.git
+# git config pull.rebase true
+# git pull origin main
